d56035c2d33b5b4ece93dbe66ed0f32ab4114595
Sahil Rajkotiya <137898902+Sahil8007@users.noreply.github.com>
2023-07-08 23:01:13 +0530

    Update aerithmatic operator.cpp

--- Prectical/aerithmatic operator.cpp
+++ Prectical/aerithmatic operator.cpp
@@ -7,10 +7,10 @@ int main()
 	scanf("%d",&s);
 	printf("Enter valau of a:");
 	scanf("%d",&a);
-	printf("Addition of s and a is : %d",s+=a);
+	printf("Multiplication of s and a is : %d",s*=a);
 	printf("\nSubstraction of s and a is : %d",s-=a);
-	printf("\nMultiplication of s and a is :%d",s*=a);
+	printf("\nAddition of s and a is :%d",s+=a);
+	printf("\nDivision of s and a is : %d",s/=a);
 	printf("\nModule of s and a is : %d",s%=a);
-	printf("\nDivison of s and a is : %d",s/=a);
 	return 0;
 }
