32a59328df3655654b05d9c40292bb1fa6cb9a35
Sahil Rajkotiya <137898902+Sahil8007@users.noreply.github.com>
2023-09-03 19:45:58 +0530

    Update Assessment.c

--- Assessment/Assessment.c
+++ Assessment/Assessment.c
@@ -1,48 +1,69 @@
-#include<stdio.h>
-int Addition(int a,int b)
+#include <stdio.h>
+void Addition(int a,int b)
 {
-	printf("Addition Is :%d",a+=b);
+	printf("\nEnter The First Number :");
+	scanf("%d",&a);
+	printf("\nEnter The Second Number :");
+	scanf("%d",&b);
+	printf("\nAddition Is :%d",a+b);
 }
-int Subtraction(int a,int b)
+void Subtraction(int a,int b)
 {
-	printf("Subtraction Of Digits Is :%d",a-b);
+	printf("\nEnter The First Number :");
+	scanf("%d",&a);
+	printf("\nEnter The Second Number :");
+	scanf("%d",&b);
+	printf("\nSubtraction Is :%d",a-b);
 }
-int Multiplication(int a,int b)
+void Divison(int a,int b)
 {
-	printf("Multiplication Of Digits Is :%d",a*b);
+	printf("\nEnter The First Number :");
+	scanf("%d",&a);
+	printf("\nEnter The Second Number :");
+	scanf("%d",&b);
+	printf("\nDivison Is :%d",a/b);
 }
-int Divison(int a,int b)
+void Multiplication(int a,int b)
 {
-	printf("Divison Of Digits Is :%d",a/b);
+	printf("\nEnter The First Number :");
+	scanf("%d",&a);
+	printf("\nEnter The Second Number :");
+	scanf("%d",&b);
+	printf("\nMultiplication Is :%d",a*b);
 }
-int main()
+int main() 
 {
-	int ch;
+    int ch;
 	int x,y;
-	printf("Enter First Number :");
-	scanf("%d",&x);
-	printf("Enter Second Number :");
-	scanf("%d",&y);
-	printf("1.Addition\n2.Subtraction\n3.Multiplication\n4.Divison");
-	printf("\nEnter Your Choice :");
-	scanf("%d",&ch);
-	switch(ch)
-	{
-		case 1:
-		Addition(x,y);
-			break;
-		case 2:
-			Subtraction(x,y);
-			break;
-			case 3:
-				Multiplication(x,y);
-				break;
-			case 4:
-				Divison(x,y);
-				break;
-		default :
-			printf("Please Enter Valid Choice...");
-			break;
-	}
-	return 0;
+    while (1){
+        printf("\nMenu\n");
+        printf("\n1. Addition");
+        printf("\n2. Subtraction");
+        printf("\n3. Multiplication");
+        printf("\n4. Divison");
+        printf("\n5. Exit\n");		
+        printf("\nEnter Your Choice:");
+        scanf("%d", &ch);
+        switch (ch) 
+		{
+            case 1:
+            	Addition(x,y);
+                break;
+            case 2:
+                Subtraction(x,y);
+                break;
+            case 3:
+                Multiplication(x,y);
+                break;
+            case 4:
+                Divison(x,y);
+                break;
+            case 5:
+            	printf("\nExiting The  Program, ThankYou...");
+            	return 0;
+            default:
+                printf("\nInvalid Choice. Please Enter A Valid Option.\n");           
+        }
+    }
+    return 0;
 }
